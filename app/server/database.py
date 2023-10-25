f9e6b13d8d0a2bce434809be7604008931fff164
Valentine Efagene <33757347+Valentine-Efagene@users.noreply.github.com>
2023-10-25 16:52:47 +0100

    feat: protect other DBs from tests

--- app/server/database.py
+++ app/server/database.py
@@ -1,22 +1,40 @@
-import os
-from motor.motor_asyncio import AsyncIOMotorClient
-from motor.motor_asyncio import AsyncIOMotorDatabase
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection, AsyncIOMotorDatabase
 from .config import settings
 
 env = settings.environment
 
 
-def get_database() -> AsyncIOMotorDatabase:
+def get_db_name(env: str) -> AsyncIOMotorDatabase:
     if env == 'DEVELOPMENT':
-        return AsyncIOMotorClient(settings.mongodb_dev_uri)[settings.mongodb_dev_db_name]
+        return settings.mongodb_dev_db_name
     elif env == 'PRODUCTION':
-        return AsyncIOMotorClient(settings.mongodb_prod_uri)[settings.mongodb_prod_db_name]
+        return settings.mongodb_prod_db_name
     else:
-        return AsyncIOMotorClient(settings.mongodb_test_uri)[settings.mongodb_test_db_name]
+        return settings.mongodb_test_db_name
 
 
-database: AsyncIOMotorDatabase = get_database()
-country_collection = database.get_collection("countries")
-state_collection = database.get_collection("states")
-lga_collection = database.get_collection("lgas")
-ethnicity_collection = database.get_collection("ethnicities")
+def get_database_and_client(env: str) -> AsyncIOMotorDatabase:
+    if env == 'DEVELOPMENT':
+        client = AsyncIOMotorClient(settings.mongodb_dev_uri)
+        database = client[settings.mongodb_dev_db_name]
+    elif env == 'PRODUCTION':
+        client = AsyncIOMotorClient(settings.mongodb_prod_uri)
+        database = client[settings.mongodb_prod_db_name]
+    else:
+        client = AsyncIOMotorClient(settings.mongodb_test_uri)
+        database = client[settings.mongodb_test_db_name]
+
+    return [client, database]
+
+
+client: AsyncIOMotorClient
+database: AsyncIOMotorDatabase
+[client, database] = get_database_and_client(env)
+db_name = get_db_name(env)
+
+country_collection: AsyncIOMotorCollection = database.get_collection(
+    "countries")
+state_collection: AsyncIOMotorCollection = database.get_collection("states")
+lga_collection: AsyncIOMotorCollection = database.get_collection("lgas")
+ethnicity_collection: AsyncIOMotorCollection = database.get_collection(
+    "ethnicities")
